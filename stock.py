0c45d678a5fc9f7e1847ac4bc7e63a99f7cdb565
Dexter <dlau26845@live.com>
2017-02-11 18:47:25 -0500

    fixed bug in buy_sell_hold()

--- stock.py
+++ stock.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
+from collections import Counter
 
 style.use('ggplot')
 
@@ -104,16 +105,61 @@ def process_data_for_labels(ticker):
     df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
 
     tickers = df.columns.values.tolist()
+    df.fillna(0, inplace=True)
     
     if ticker in tickers:
-        df.fillna(0, inplace=True)
-
         for i in range(1, hm_days+1):
-            df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-1) - df[ticker]) / df[ticker]
+            df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
 
         df.fillna(0, inplace=True) 
     else:
         print('process_data_for_labels, ERROR: ' + ticker + ' does not exist')
 
     return tickers, df
+
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+
+    # 1 = buy, -1 = sell, 0 = hold
+    for col in cols:
+        if col > requirement:
+            return 1
+        if col < -requirement:
+            return -1
+
+    return 0
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)]
+                                               ))
+
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+    df.fillna(0, inplace=True)
+
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+extract_featuresets('XOM')
+
 
