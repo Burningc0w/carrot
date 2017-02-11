b173e775faf19d534697420aafe1d5bf6118318f
Dexter <dlau26845@live.com>
2017-02-11 10:13:43 -0500

    Added visualize_data()

--- stock.py
+++ stock.py
@@ -1,6 +1,7 @@
 import bs4 as bs
 import datetime as dt
 import os
+import numpy as np
 import pandas as pd
 import pandas_datareader.data as web
 import pickle
@@ -69,4 +70,27 @@ def compile_data():
     print(main_df.head())
     main_df.to_csv('sp500_joined_closes.csv')
 
-compile_data()
+def visualize_data():
+    df = pd.read.csv('sp500_joined_closes.csv')
+    df_corr = df.corr()
+    print(df_corr.head())
+    ax = fig.add_subplot(1,1,1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arrange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arrange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1,1)
+    plt.tight_layout()
+    plt.show()
+
+visualize_data()
